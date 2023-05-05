4bf0c173db013cb81a6126136670ae866e28a42f
Mogwai <diane.roberg@gmail.com>
2023-05-05 12:09:41 +0200

    fignolage

--- js/script.js
+++ js/script.js
@@ -5,4 +5,15 @@ iptPw.addEventListener('input', () => {
     iptPwd.content = iptPw.value;
     iptPwd.checkPasswordBar(pwBar);
     score.innerText = iptPwd.score;
+
+    // màj des indications sur la page 
+    // (-> passe en gras quand c'est validé, en normal quand c'est plus bon)
+    for (let criteria in iptPwd.checked) {
+        if(iptPwd.checked[criteria]['checked']) {
+            document.getElementById(criteria).style.fontWeight = 'bold';
+        }
+        else {
+            document.getElementById(criteria).style.fontWeight = 'normal';
+        }
+    }
 });
